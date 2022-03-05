9eb224cb620c94f0e078ad233254593c5e7847c4
websmartdream <webdream1108@gmail.com>
2022-03-05 14:17:45 +0800

    remove the warning

--- src/layouts/sidebar/Sidebar.tsx
+++ src/layouts/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { RiDashboardFill, RiUser2Fill, RiMessage2Fill } from "react-icons/ri";
