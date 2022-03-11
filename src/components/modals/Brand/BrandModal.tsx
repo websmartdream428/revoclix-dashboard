b86ae6d3a894edc05269da382aee2c7df4fc8a64
websmartdream <webdream1108@gmail.com>
2022-03-11 09:36:59 +0800

    feat: create brand modal

--- src/components/modals/Brand/BrandModal.tsx
+++ src/components/modals/Brand/BrandModal.tsx
@@ -23,7 +23,7 @@ const BrandModal: React.FC<ModalProps> = ({
   };
   return (
     <Modal
-      title="Category"
+      title="Brand"
       centered
       visible={visible}
       width={"90%"}
@@ -36,66 +36,24 @@ const BrandModal: React.FC<ModalProps> = ({
         wrapperCol={{ span: 20 }}
         layout="horizontal"
       >
-        <Form.Item label="*Name">
+        <Form.Item label="* Name">
           <Input />
+          <span>{"Invalid characters: <>;=#{}"}</span>
+        </Form.Item>
+
+        <Form.Item label="Logo">
+          <Upload name="logo" action="/upload.do" listType="picture">
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+
+          <span>{"Upload a brand logo from your computer."}</span>
+        </Form.Item>
+        <Form.Item label="Meta title">
+          <Input placeholder="To have a different title from the category name, enter it here." />
           <span>{"Invalid characters: <>;=#{}"}</span>
         </Form.Item>
         <Form.Item label="Displayed">
           <Switch />
-        </Form.Item>
-        <Form.Item label="Category cover image">
-          <Upload name="logo" action="/upload.do" listType="picture">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-        </Form.Item>
-        <Form.Item label="Category thumbnail">
-          <Upload name="logo" action="/upload.do" listType="picture">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-          <span>{"Displays a small image in the parent category's page."}</span>
-        </Form.Item>
-        <Form.Item label="Menu thumbnails">
-          <Upload name="logo" action="/upload.do" listType="picture">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-          <span>
-            {
-              "The category thumbnail appears in the menu as a small image representing the category"
-            }
-          </span>
-        </Form.Item>
-        <Form.Item label="Meta title">
-          <Input placeholder="To have a different title from the category name, enter it here." />
-          <span>{"0 of 70 characters used (recommended)"}</span>
-        </Form.Item>
-        <Form.Item label="Meta description">
-          <Input.TextArea placeholder="To have a different description than your category summary in search results page, write it here." />
-          <span>{"0 of 160 characters used (recommended)"}</span>
-        </Form.Item>
-        <Form.Item label="Meta keywords">
-          <Select
-            mode="multiple"
-            allowClear
-            style={{ width: "100%" }}
-            placeholder="Add tag"
-            defaultValue={["a10", "c12"]}
-            onChange={handleChange}
-          >
-            {children}
-          </Select>
-          <span>
-            {
-              "To add tags, click in the field, write something, and then press the 'Enter' key. Invalid characters: <>;=#{}"
-            }
-          </span>
-        </Form.Item>
-        <Form.Item label="* Friendly URL">
-          <Input />
-          <span>
-            {
-              "Only letteres, numbers, underscore(_) and the minus(-) character are allowed."
-            }
-          </span>
         </Form.Item>
       </Form>
     </Modal>
